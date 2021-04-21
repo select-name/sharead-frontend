7d0a6eee3b0e5478f787eb14cfac35899051e35a
Ilya Azin <martis.azin@gmail.com>
2021-04-21 07:44:35 +0300

    feat(home): add base layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { Layout } from "antd";
+import cn from "classnames";
 import Routing from "pages";
 import styles from "./styles.module.scss";
+import "./index.scss";
 
 function App() {
     return (
-        <div className={styles.root}>
-            <Routing />
+        <div className={styles.h100}>
+            <Layout className={styles.h100}>
+                <Layout.Header className={styles.header}>
+                    <h1>sharead</h1>
+                </Layout.Header>
+                <Layout.Content className={cn(styles.h100, styles.content)}>
+                    <Routing />
+                </Layout.Content>
+                <Layout.Footer className={styles.footer}>
+                    Sharead ©2021 Created by SelectName team
+                </Layout.Footer>
+            </Layout>
         </div>
     );
 }
