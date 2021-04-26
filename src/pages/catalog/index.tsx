1f367b2f730e615eab71a5ad0e3081d84ce109bf
Ilya Azin <martis.azin@gmail.com>
2021-04-26 16:37:24 +0300

    feat(catalog): normalize layout
    SL-22

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, Card, Empty } from "antd";
+import { Typography, Card, Empty, Col, Row, Layout } from "antd";
 import { BookFilled } from "@ant-design/icons";
 
 // FIXME: resolve better params usage
@@ -9,50 +9,56 @@ import * as fapi from "shared/fixtures";
 import styles from "./styles.module.scss";
 
 // !!! FIXME: split by features!
+// TODO: Add skeletons loader
 
 /**
  * @page Каталог книг
  */
+// eslint-disable-next-line max-lines-per-function
 const CatalogPage = () => {
     const params = headerParams.useSearchParam();
 
     const query = fapi.books.getList({ search: params.search });
 
     return (
-        <>
+        <Layout>
             <Header />
             <div className={styles.root}>
                 <Typography.Title className={styles.title} level={2}>
                     Каталог книг
                 </Typography.Title>
-                <div className={styles.catalog}>
-                    <div className={styles.grid}>
-                        {query.map((b) => (
-                            <Card
-                                key={b.id}
-                                hoverable
-                                style={{ width: 400 }}
-                                cover={<BookFilled style={{ fontSize: 100, paddingTop: 20 }} />}
-                                className={styles.gridItem}
-                            >
-                                <Card.Meta
-                                    title={b.name}
-                                    description={b.authors
-                                        .map(fapi.authors.getShortname)
-                                        .join(", ")}
-                                />
-                            </Card>
-                        ))}
-                    </div>
-                    {query.length === 0 && (
-                        <Empty
-                            className={styles.placeholder}
-                            description="Не удалось найти книги по вашему запросу"
-                        />
-                    )}
-                </div>
+                <Layout className={styles.catalog}>
+                    <Layout.Content>
+                        <div className={styles.grid}>
+                            {query.map((b) => (
+                                <Card
+                                    key={b.id}
+                                    hoverable
+                                    style={{ width: "30%" }}
+                                    headStyle={{ background: "grey" }}
+                                    cover={<BookFilled className={styles.gridItemImgPlaceholder} />}
+                                    className={styles.gridItem}
+                                >
+                                    <Card.Meta
+                                        title={b.name}
+                                        description={b.authors
+                                            .map(fapi.authors.getShortname)
+                                            .join(", ")}
+                                    />
+                                </Card>
+                            ))}
+                        </div>
+                        {query.length === 0 && (
+                            <Empty
+                                className={styles.catalogPlaceholder}
+                                description="Не удалось найти книги по вашему запросу"
+                            />
+                        )}
+                    </Layout.Content>
+                    <Layout.Sider width={400}>Фильтры</Layout.Sider>
+                </Layout>
             </div>
-        </>
+        </Layout>
     );
 };
 
