56346d2829f43a2bf11a5204524a0f06dd891d62
Ilya Azin <martis.azin@gmail.com>
2021-04-26 07:00:57 +0300

    feat(catalog): add catalog page basely
    SL-22

---
+++ src/pages/catalog/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * @page Каталог книг
+ */
+const CatalogPage = () => {
+    return <div>Список книг</div>;
+};
+
+export default CatalogPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 const HomePage = lazy(() => import("./home"));
 const ProfilePage = lazy(() => import("./profile"));
+const CatalogPage = lazy(() => import("./catalog"));
 
 // TODO: Add auth zone restricting
 // TODO: Add query-params provider
@@ -13,6 +14,7 @@ const Routing = () => {
         <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/catalog" component={CatalogPage} />
             <Redirect to="/" />
         </Switch>
     );
