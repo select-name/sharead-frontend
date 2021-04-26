1801101727e20279c39f4181a3aa0d011f3aa4e7
Ilya Azin <martis.azin@gmail.com>
2021-04-27 01:25:17 +0300

    feat(catalog): prototype index-page
    SL-22

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
+const IndexPage = lazy(() => import("./index/index"));
 const AboutPage = lazy(() => import("./about"));
 const ProfilePage = lazy(() => import("./profile"));
 const CatalogPage = lazy(() => import("./catalog"));
@@ -17,12 +18,13 @@ export const headerIgnoreRoutes = ["/debug/no-header"];
 const Routing = () => {
     return (
         <Switch>
+            <Route exact path="/" component={IndexPage} />
             <Route exact path="/about" component={AboutPage} />
             <Route exact path="/profile" component={ProfilePage} />
             <Route exact path="/catalog" component={CatalogPage} />
             <Route exact path="/debug/no-header" component={NoHeaderPage} />
             {/* FIXME: temp! (redirect later to main home page) */}
-            <Redirect to="/catalog" />
+            <Redirect to="/" />
         </Switch>
     );
 };
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,64 @@
+import { Carousel, Typography, Layout, Skeleton, Row, Col } from "antd";
+
+import { Header } from "features/header";
+import styles from "./styles.module.scss";
+
+// !!! FIXME: split by features!
+// TODO: Add skeletons loader
+
+/**
+ * @page Индексовая страница
+ */
+// eslint-disable-next-line max-lines-per-function
+const IndexPage = () => {
+    return (
+        <Layout>
+            <Header />
+            <main className={styles.root}>
+                <Typography.Title className={styles.title} level={2}>
+                    Букшеринг
+                </Typography.Title>
+                <section className={styles.carousel}>
+                    <Carousel autoplay>
+                        <div className={styles.carouselItem}>
+                            <Typography.Title level={3}>BANNER-1</Typography.Title>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <Typography.Title level={3}>BANNER-2</Typography.Title>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <Typography.Title level={3}>BANNER-3</Typography.Title>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <Typography.Title level={3}>BANNER-4</Typography.Title>
+                        </div>
+                    </Carousel>
+                </section>
+                <section>
+                    <Typography.Title className={styles.title} level={2}>
+                        Категории книг
+                    </Typography.Title>
+                    <Row className={styles.skeletonZone}>
+                        <Col span={6} offset={1}>
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                        </Col>
+                        <Col span={6} offset={1}>
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                        </Col>
+                        <Col span={6} offset={1}>
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                            <Skeleton.Input className={styles.skeletonItem} size="large" active />
+                        </Col>
+                    </Row>
+                </section>
+            </main>
+        </Layout>
+    );
+};
+
+export default IndexPage;
