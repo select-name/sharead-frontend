3aea1537e65003a854cbd7218ed30f9f30660943
Ilya Azin <martis.azin@gmail.com>
2021-05-01 18:49:29 +0300

    feat(index-page): add location alert
    SL-24

--- src/features/header/ui/index.tsx
+++ src/features/header/ui/index.tsx
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import { Layout, Badge } from "antd";
+import { Layout, Badge, Alert } from "antd";
 import {
     HeartOutlined,
     ShoppingCartOutlined,
@@ -57,42 +57,54 @@ const actions = [
     },
 ];
 
+const LocationAlert = () => (
+    <Alert
+        className={styles.locationAlert}
+        message="На данный момент, сервис доступен только для Казани"
+        type="info"
+        closeText="Закрыть"
+    />
+);
+
 // const NOT_AVAILABLE = "Пока что данный функционал не доступен";
 
 const Header = (props: Props) => {
     const { className } = props;
 
     return (
-        <Layout.Header className={cn(styles.root, className)}>
-            <Link className={styles.logo} to="/">
-                <Logo width={24} />
-                <h1 className={styles.logoTitle}>sharead</h1>
-            </Link>
-            <div className={styles.search}>
-                <Search />
-            </div>
-            {/* TODO: add amount-label later */}
-            <div className={styles.toolbar}>
-                {actions.map(({ label, Icon, url, showBadge, disabled }) => (
-                    <Link
-                        key={label}
-                        to={url}
-                        className={cn(styles.toolbarAction, {
-                            [styles.toolbarActionDisabled]: disabled,
-                        })}
-                        // title={disabled ? NOT_AVAILABLE : ""}
-                    >
-                        {/* Для выравнивания бейджа */}
-                        <span className={styles.toolbarActionIcon}>
-                            <Badge dot={showBadge} style={{ backgroundColor: "#108ee9" }}>
-                                <Icon style={{ fontSize: 24 }} />
-                            </Badge>
-                        </span>
-                        <span>{label}</span>
-                    </Link>
-                ))}
-            </div>
-        </Layout.Header>
+        <>
+            <LocationAlert />
+            <Layout.Header className={cn(styles.root, className)}>
+                <Link className={styles.logo} to="/">
+                    <Logo width={24} />
+                    <h1 className={styles.logoTitle}>sharead</h1>
+                </Link>
+                <div className={styles.search}>
+                    <Search />
+                </div>
+                {/* TODO: add amount-label later */}
+                <div className={styles.toolbar}>
+                    {actions.map(({ label, Icon, url, showBadge, disabled }) => (
+                        <Link
+                            key={label}
+                            to={url}
+                            className={cn(styles.toolbarAction, {
+                                [styles.toolbarActionDisabled]: disabled,
+                            })}
+                            // title={disabled ? NOT_AVAILABLE : ""}
+                        >
+                            {/* Для выравнивания бейджа */}
+                            <span className={styles.toolbarActionIcon}>
+                                <Badge dot={showBadge} style={{ backgroundColor: "#108ee9" }}>
+                                    <Icon style={{ fontSize: 24 }} />
+                                </Badge>
+                            </span>
+                            <span>{label}</span>
+                        </Link>
+                    ))}
+                </div>
+            </Layout.Header>
+        </>
     );
 };
 
