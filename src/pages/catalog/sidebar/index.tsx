818fcfaeca86518de0f31571c4b6fb900b29334f
Ilya Azin <martis.azin@gmail.com>
2021-05-25 15:08:28 +0300

    feat(catalog): remove owner section

--- src/pages/catalog/sidebar/index.tsx
+++ src/pages/catalog/sidebar/index.tsx
@@ -14,7 +14,6 @@ const CatalogSidebar = () => {
 
                 <PriceSection />
                 <TimeSection />
-                <OwnerSection />
                 <CategorySection />
                 <AuthorSection />
                 <PublisherSection />
