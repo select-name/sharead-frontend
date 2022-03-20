03d46842b2133a9be62adc85f72a4136b1c3c498
Ilya Azin <martis.azin@gmail.com>
2022-03-20 15:12:10 +0300

    chore(metrics): re-setup yametrika

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,23 @@
+import { YMInitializer } from "react-yandex-metrika";
 import Routing from "pages";
 import { withHocs } from "./hocs";
 import "./index.scss";
 
+const yaMetrikaOptions = {
+    defer: true,
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+    ecommerce: "dataLayer",
+};
+
 // FIXME: Возможно, понабодится более сложная разметка позднее
-const App = () => <Routing />;
+const App = () => (
+    <>
+        <YMInitializer accounts={[87502056]} options={yaMetrikaOptions} />
+        <Routing />
+    </>
+);
 
 export default withHocs(App);
